55e6536ac4187db30309d34ef400832415755af5
Himu Project <lemonpatwari@gmail.com>
2019-01-15 21:26:52 +0600

    login title change

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentelella Alela! | </title>
+    <title>Lemon </title>
 
     <!-- Bootstrap -->
     <link href="{{asset('/')}}assets/admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
